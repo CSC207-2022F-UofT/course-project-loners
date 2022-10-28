538695e1a09c0898b136d4038568ac66147f44da
Amelia Riddell <amelia.riddell@mail.utoronto.ca>
2022-10-28 16:38:12 -0400

    profile and preferences entities.

---
+++ src/main/java/project/Preferences.java
@@ -0,0 +1,37 @@
+package project;
+
+public class Preferences {
+    private int preferredAge;
+    private String preferredGender;
+    private int[] preferredLocation;
+
+    public Preferences(int preferredAge, String preferredGender, int[] preferredLocation) {
+        this.preferredAge = preferredAge;
+        this.preferredGender = preferredGender;
+        this.preferredLocation = preferredLocation;
+    }
+
+    public int getPreferredAge() {
+        return preferredAge;
+    }
+
+    public void setPreferredAge(int preferredAge) {
+        this.preferredAge = preferredAge;
+    }
+
+    public String getPreferredGender() {
+        return preferredGender;
+    }
+
+    public void setPreferredGender(String preferredGender) {
+        this.preferredGender = preferredGender;
+    }
+
+    public int[] getPreferredLocation() {
+        return preferredLocation;
+    }
+
+    public void setPreferredLocation(int[] preferredLocation) {
+        this.preferredLocation = preferredLocation;
+    }
+}
