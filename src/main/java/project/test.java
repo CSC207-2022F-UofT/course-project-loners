585098a4cbfaf0ed604e99279c9969533d7366e8
Rick0317 <rick.hey.huang@gmail.com>
2022-11-04 16:33:03 -0400

    Changed some files

---
+++ src/main/java/project/test.java
@@ -0,0 +1,9 @@
+package project;
+
+import java.io.File;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println(new File("credential.json").getAbsolutePath());
+    }
+}
