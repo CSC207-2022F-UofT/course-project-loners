1f053cfc1bfe28f5e29e7fccf3adea5309e3edaf
Amelia Riddell <amelia.riddell@mail.utoronto.ca>
2022-11-04 16:35:47 -0400

    made a few updates to the GiveNotification class because I need to have one profile and a list of others.

--- src/main/java/Use_Case/GiveNotification.java
+++ src/main/java/Use_Case/GiveNotification.java
@@ -7,23 +7,25 @@ import java.util.List;
 
 public class GiveNotification {
     private Profile profile1;
-    private Profile profile2;
+    private List<Profile> profiles;
 
-    private void setProfiles(Profile profile1, Profile profile2) {
+    private void setProfiles(Profile profile1, List<Profile> profile2) {
         this.profile1 = profile1;
-        this.profile2 = profile2;
+        this.profiles = profile2;
     }
 
-    public String[] notif(Profile me, Profile you){
-        setProfiles(me, you);
+    public String[] notif(Profile me, List<Profile> yous){
+        setProfiles(me, yous);
         //Returns null if we do not have a match, return an array with match indicator and notif if a match occurs
         //between profiles
         // We do this because we want to know if we have a match and we want the match message to show in the inbox
-        List<String> likes2 = profile2.getLikes();
-        //profile1 is our profile while profile2 is the other profile
-        for (String emails : likes2){
-            if (profile1.getEmail().equals(emails)){
-                return new String[] {"match", "You have a match with " + profile2.getName() + "!"};
+        for (Profile profile : profiles) {
+            List<String> likes2 = profile.getLikes();
+            //profile1 is our profile while profile2 is the other profile
+            for (String emails : likes2) {
+                if (profile1.getEmail().equals(emails)) {
+                    return new String[]{"match", "You have a match with " + profile.getName() + "!"};
+                }
             }
         }
         return null;
