a9e67cb5a691f7906c7093f4deea6c49248ebc62
Rick0317 <rick.hey.huang@gmail.com>
2022-11-09 11:24:56 -0500

    Created a database file and a simple implementation of EditProfileControl to modify the database. Plus, DataFetchSend (interface)

--- src/main/java/project/test.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-import java.io.File;
-
-public class test {
-    public static void main(String[] args){
-        System.out.println(new File("credential.json").getAbsolutePath());
-    }
-}
