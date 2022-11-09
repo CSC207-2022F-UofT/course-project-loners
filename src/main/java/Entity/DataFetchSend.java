c7fb42a47227793763d3afc156a8fd9ee3d0bb6f
Rick0317 <rick.hey.huang@gmail.com>
2022-11-09 11:24:56 -0500

    Created a database file and a simple implementation of EditProfileControl to modify the database. Plus, DataFetchSend (interface)

--- src/main/java/Entity/DataFetchSend.java
+++ src/main/java/Entity/DataFetchSend.java
@@ -3,10 +3,10 @@ package Entity;
 import java.util.Set;
 
 public interface DataFetchSend {
-    public default Set<Object> fetch() {
+    default Set<Object> fetch() {
         return null;
     }
-    public default boolean send(){
+    default boolean send(){
         return true;
     }
 }
