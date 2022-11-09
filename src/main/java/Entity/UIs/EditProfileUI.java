99c00b3123b182c2a1bf9ce16351c8262ce169aa
Rick0317 <rick.hey.huang@gmail.com>
2022-11-09 11:31:22 -0500

    Created a database file and a simple implementation of EditProfileControl to modify the database. Plus, DataFetchSend (interface)

---
+++ src/main/java/Entity/UIs/EditProfileUI.java
@@ -0,0 +1,15 @@
+package Entity.UIs;
+import Entity.EditProfileControl;
+
+import javax.swing.*;
+
+public class EditProfileUI {
+    JFrame f;
+    public EditProfileUI(){
+
+    }
+    public static void main(String[] args) {
+        new EditProfileUI();
+
+    }
+}
---
+++ src/main/java/Entity/UIs/MyProfileUI.java
@@ -0,0 +1,26 @@
+package Entity.UIs;
+import Entity.EditProfileControl;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyProfileUI {
+    JFrame f;
+    Color red  = new Color(255, 0, 0);
+    public MyProfileUI (){
+        f=new JFrame();
+
+        JButton b=new JButton("click");
+        b.setBounds(130,100,100, 40);
+        f.add(b);
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new MyProfileUI();
+        EditProfileControl control = new EditProfileControl("Rick");
+        control.send();
+    }
+}
+
