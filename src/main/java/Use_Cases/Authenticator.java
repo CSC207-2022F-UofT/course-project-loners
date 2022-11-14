4f16ac1fac57e7497177ca9da2433f9b2dede3f5
siyounkim <siyoun.kim02@gmail.com>
2022-11-14 09:36:36 -0500

    I created two use_case classes for Authenticator and PreferredLocationConnector.

--- src/main/java/Use_Cases/Authenticator.java
+++ src/main/java/Use_Cases/Authenticator.java
@@ -1,46 +1,5 @@
-package Use_Cases;
+package Use_Case;
 
-import java.awt.image.BufferedImage;
-import java.util.List;
+public class Authenticator {
 
-public interface Authenticator {
-    public default boolean is_valid_email(String email){
-        return true;
-    }
-
-    public default boolean is_valid_name(String name){
-        return true;
-    }
-
-    public default boolean is_valid_age(int age){
-        return true;
-    }
-
-    public default boolean is_valid_gender(String gender){
-        return true;
-    }
-
-    public default boolean is_valid_orientation(String orientation){
-        return true;
-    }
-
-    public default boolean is_valid_location(int[] location){
-        return true;
-    }
-
-    public default boolean is_valid_image(BufferedImage image){
-        return true;
-    }
-
-    public default boolean is_valid_bio(String bio){
-        return true;
-    }
-
-    public default boolean is_valid_hobbies(List<String> hobbies){
-        return true;
-    }
-
-    public default boolean is_valid_socialMedia(String socialMedia){
-        return true;
-    }
 }
