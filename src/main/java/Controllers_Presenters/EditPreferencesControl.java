a440db84b6f199516295187fffa21294b469d4ca
jamie-yuan <jamie.q.yuan@gmail.com>
2022-11-14 15:00:19 -0500

    Removed the Profile object as an instance attribute in the Preferences class

--- src/main/java/Controllers_Presenters/EditPreferencesControl.java
+++ src/main/java/Controllers_Presenters/EditPreferencesControl.java
@@ -3,9 +3,9 @@ package Controllers_Presenters;
 import java.util.List;
 
 public class EditPreferencesControl {
-    private String prefAge;
-    private String prefGender;
-    private String prefLocation;
+    private static String prefAge;
+    private static String prefGender;
+    private static String prefLocation;
 
     // arguments for constructor passed in by EditPreferencesUI
     public EditPreferencesControl(List<String> preferences) {
@@ -15,7 +15,6 @@ public class EditPreferencesControl {
     }
 
     public static void setPreferences() {
-        // find the Profile object using DataFetchSend to get the corresponding user
         // create a new Preferences object with parameters of the appropriate type
     }
 
--- src/main/java/Entities/Preferences.java
+++ src/main/java/Entities/Preferences.java
@@ -4,13 +4,11 @@ public class Preferences {
     private int preferredAge;
     private String preferredGender;
     private double[] preferredLocation;
-    private Profile user;
 
-    public Preferences(int preferredAge, String preferredGender, double[] preferredLocation, Profile user) {
+    public Preferences(int preferredAge, String preferredGender, double[] preferredLocation) {
         this.preferredAge = preferredAge;
         this.preferredGender = preferredGender;
         this.preferredLocation = preferredLocation;
-        this.user = user;
     }
 
     public int getPreferredAge() {
