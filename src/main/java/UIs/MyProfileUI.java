672b3b7c508fd0bdfefab243313078924e4e5300
Rick0317 <rick.hey.huang@gmail.com>
2022-11-16 22:08:20 -0500

    Add ToBuffer file which converts an image to BufferedImage

--- src/main/java/UIs/MyProfileUI.java
+++ src/main/java/UIs/MyProfileUI.java
@@ -1,13 +1,33 @@
 package UIs;
 
+import Use_Cases.ToBuffer;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MyProfileUI {
     JFrame f;
     Color red  = new Color(255, 0, 0);
+    FileDialog fd = new FileDialog(f, "Open", FileDialog.LOAD);
+
+
     public MyProfileUI (){
         f=new JFrame();
+
+        try{
+            Image image = ImageIO.read(new File(fd.getFile()));
+            ToBuffer toBuffer = new ToBuffer();
+            BufferedImage bimage = toBuffer.toBufferedImage(image);
+            bimage.toString();
+        } catch (IOException e){
+            System.out.println("Something went wrong");
+        }
+
+
 
         JButton b=new JButton("click");
         JTextField textField = new JTextField(20);
