763e38917b35e5033bb09b50d8e93fddd5546c6d
kellyfan <kani.fan@mail.utoronto.ca>
2022-11-18 00:15:10 -0500

    1. Added a constrcutor to Profile.java for the registration feature. 2. Plan changed: WelcomeControl deleted as it merged with WelcomeUI; ProfileGenerator changed to RegDataStore. 3. Implemented WelcomeUI.java, RegUI.java, RegControl.java, RegDataStore.java, RegChecker.java
    4. Added tmp_DataFetch.java and tmp_DataSend.java for RegDataStore and RegChecker; will change/delete after tomorrow's discussion.

---
+++ src/main/java/Controllers_Presenters/RegControl.java
@@ -0,0 +1,32 @@
+package Controllers_Presenters;
+
+import UI.LogUI;
+import Use_Case.RegChecker;
+import Use_Case.RegDataStore;
+
+import javax.swing.*;
+
+public class RegControl {
+    public RegControl(String email, String password, String name, String age, String gender, String postcode, JFrame regf){
+        RegChecker checker = new RegChecker(email, password, name, age, gender, postcode);
+        if (!checker.pass){ // if checker did not pass, show failure message
+            checker_message_presenter(checker);
+        } else { // if checker passed, store the data
+            int age_int = Integer.parseInt(age);
+            RegDataStore store_data = new RegDataStore(email, password, name, age_int, gender, postcode);
+            if (store_data.success){ // if data stored successfully, show success message and go to login page
+                JOptionPane.showMessageDialog(null,"Registration done. Click OK to the login page.");
+                regf.setVisible(false);
+                LogUI logui = new LogUI();
+                logui.setVisible(true);
+            } else{ // if data had not been not stored, show wrong message
+                JOptionPane.showMessageDialog(null, "Something went wrong with the registration.");
+            }
+        }
+    }
+
+    public void checker_message_presenter(RegChecker checker){
+        String message = "Warning: \n" + checker.diagnose + "please try again!";
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
