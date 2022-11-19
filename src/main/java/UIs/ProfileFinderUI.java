5cf15dd95376851a28b6f1661ab0c5f10578f9e9
Amelia Riddell <amelia.riddell@mail.utoronto.ca>
2022-11-19 13:30:44 -0500

    Made some labels for the ProfileFinderUI.

--- src/main/java/UIs/ProfileFinderUI.java
+++ src/main/java/UIs/ProfileFinderUI.java
@@ -1,5 +1,19 @@
 package UIs;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ProfileFinderUI {
     // This class will show a profile on the screen based on the matching algorithm
+    JFrame frame = new JFrame();
+    GridLayout layout = new GridLayout(5, 2, 10, 20);
+    JLabel nameLabel = new JLabel("Name: ");
+    JLabel genderLabel = new JLabel("Gender: ");
+    JLabel ageLabel = new JLabel("Age: ");
+    JLabel bioLabel = new JLabel("Bio: ");
+
+    JButton likeButton = new JButton("Like");
+    JButton passButton = new JButton("Pass");
+
+
 }
