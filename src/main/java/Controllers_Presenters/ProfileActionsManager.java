cdeafc65ea9daf8239c6454dca3cd8413f4d0131
Amelia Riddell <amelia.riddell@mail.utoronto.ca>
2022-11-19 13:44:23 -0500

    Added ProfileActionsManager class to return the profile connections.

--- src/main/java/Controllers_Presenters/ProfileActionsManager.java
+++ src/main/java/Controllers_Presenters/ProfileActionsManager.java
@@ -1,4 +1,17 @@
 package Controllers_Presenters;
+import Controllers_Presenters.ConnectProfilesControl;
+
+import java.util.List;
 
 public class ProfileActionsManager {
+
+    int myId;
+    public ProfileActionsManager(int myId) {
+        this.myId = myId;
+    }
+
+    public List<String> ListOfConnections(){
+        ConnectProfilesControl c = new ConnectProfilesControl(myId);
+        return c.passPreferences();
+    }
 }
