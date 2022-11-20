fb40ecf9772f5705f4bca73fbf325fa2fc76e48c
kellyfan <kani.fan@mail.utoronto.ca>
2022-11-20 17:16:36 -0500

    1. Modified the database.txt(delete empty lines) 2. Added MainUI.java 2. In DataFetchControl, made fetch_id_fromEmail return static 3. In EditPreferencesUI and MyProfileUI, added a method setVisible so that we can open up the window in the MainUI

--- src/main/java/UIs/ProfileFinderUI.java
+++ src/main/java/UIs/ProfileFinderUI.java
@@ -1,5 +1,7 @@
 package UIs;
 
 public class ProfileFinderUI {
+    public ProfileFinderUI(String email) {
+    }
     // This class will show a profile on the screen based on the matching algorithm
 }
