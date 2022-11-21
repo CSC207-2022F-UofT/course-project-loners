64792cc2764f3bc3169e32717d55e5a3d541320d
jamie-yuan <jamie.q.yuan@gmail.com>
2022-11-21 16:36:52 -0500

    Fixed the new Preferences constructor

--- src/main/java/Entities/Preferences.java
+++ src/main/java/Entities/Preferences.java
@@ -13,11 +13,9 @@ public class Preferences {
         this.id = id;
     }
 
-    public Preferences(int preferredAge, String preferredGender,
-                       double preferredLocationRange) {
+    public Preferences(int preferredAge, String preferredGender, double preferredLocationRange) {
         this.preferredAge = preferredAge;
         this.preferredGender = preferredGender;
-        this.preferredLocation = new double[]{0.0, 0.0};
         this.preferredLocationRange = preferredLocationRange;
         this.id = 0;
     }
