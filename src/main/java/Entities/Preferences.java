0f44b49d1ce3941e32ab18242d6b84a55316b328
Amelia Riddell <amelia.riddell@mail.utoronto.ca>
2022-11-27 15:19:28 -0500

    Added some comments, did some refactoring.

--- src/main/java/Entities/Preferences.java
+++ src/main/java/Entities/Preferences.java
@@ -1,11 +1,21 @@
 package Entities;
 
+/**
+ * Class for the Preferences entity: what the user prefers to date
+ */
 public class Preferences {
-    private int preferredAge;
-    private String preferredGender;
-    private double preferredLocationRange;
-    private int id;
+    private final int preferredAge;
+    private final String preferredGender;
+    private final double preferredLocationRange;
+    private final int id;
 
+    /**
+     * Constructor for the Preferences class
+     * @param preferredAge user's preferred age
+     * @param preferredGender user's preferred gender
+     * @param preferredLocationRange user's preferred location range
+     * @param id the user's id
+     */
     public Preferences(int preferredAge, String preferredGender, double preferredLocationRange, int id) {
         this.preferredAge = preferredAge;
         this.preferredGender = preferredGender;
@@ -13,25 +23,30 @@ public class Preferences {
         this.id = id;
     }
 
-    public Preferences(int preferredAge, String preferredGender, double preferredLocationRange) {
-        this.preferredAge = preferredAge;
-        this.preferredGender = preferredGender;
-        this.preferredLocationRange = preferredLocationRange;
-        this.id = 0;
-    }
-
+    /**
+     * @return user's preferred age
+     */
     public int getPreferredAge() {
         return preferredAge;
     }
 
+    /**
+     * @return user's preferred gender
+     */
     public String getPreferredGender() {
         return preferredGender;
     }
 
+    /**
+     * @return user's preferred location range
+     */
     public double getPreferredLocationRange() {
         return preferredLocationRange;
     }
 
+    /**
+     * @return the user's id
+     */
     public int getID() {
         return id;
     }
--- src/main/java/Entities/Profile.java
+++ src/main/java/Entities/Profile.java
@@ -4,6 +4,9 @@ import java.awt.image.BufferedImage;
 import java.util.List;
 import Use_Cases.Swipe;
 
+/**
+ * An entity to store the attributes of the user's profile, or information about the user
+ */
 public class Profile implements Swipe{
     private String email;
     private String name;
@@ -18,11 +21,28 @@ public class Profile implements Swipe{
     private String socialMedia;
     private List<String> likes;
 
+    /**
+     * Constructor for the user's profile
+     * @param email the user's email
+     * @param password the user's password
+     */
     public Profile(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
+    /**
+     * Constructor for the Profile Class
+     * @param name user's name
+     * @param age user's age, from 1 to 100
+     * @param gender user's gender, either male, female, other
+     * @param orientation user's sexual orientation, straight or gay
+     * @param location the user's coordinates
+     * @param image the user's image
+     * @param bio the user's biography
+     * @param hobbies a list of the user's hobbies
+     * @param socialMedia the user's social media profile handle
+     */
     public Profile(String name, int age, String gender, String orientation, double[] location, BufferedImage image,
                    String bio, List<String> hobbies, String socialMedia) {
         this.name = name;
@@ -36,6 +56,21 @@ public class Profile implements Swipe{
         this.socialMedia = socialMedia;
     }
 
+    /**
+     * Constructor for the Profile Class
+     * @param name user's name
+     * @param age user's age, from 1 to 100
+     * @param gender user's gender, either male, female, other
+     * @param orientation user's sexual orientation, straight or gay
+     * @param location the user's coordinates
+     * @param image the user's image
+     * @param bio the user's biography
+     * @param hobbies a list of the user's hobbies
+     * @param socialMedia the user's social media profile handle
+     * @param email the user's email, has to have @
+     * @param likes the list of other profile ids that the user had liked
+     * @param password the user's password
+     */
     public Profile(String name, int age, String gender, String orientation, double[] location, BufferedImage image,
                    String bio, List<String> hobbies, String socialMedia, List<String> likes, String email,
                    String password) {
@@ -53,6 +88,15 @@ public class Profile implements Swipe{
         this.password = password;
     }
 
+    /**
+     * Constructor for the profile class
+     * @param email the user's email
+     * @param password the user's password
+     * @param name the user's name
+     * @param age the user's age, from 1 to 100
+     * @param gender the user's gender, male, female, or other
+     * @param location the user's location in coordinates
+     */
     public Profile(String email, String password, String name, int age, String gender, double[] location) {
         this.email = email;
         this.password = password;
@@ -62,103 +106,195 @@ public class Profile implements Swipe{
         this.location = location;
     }
 
+    /**
+     * add an email to our list of likes
+     * @param email the email of the other user that is liked
+     */
     @Override
     public void addLike(String email){
         likes.add(email);
     }
 
+    /**
+     * a getter for the email
+     * @return user's email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * A setter for the email
+     * @param email the user's email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * a getter for the name
+     * @return the user's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * a setter for the name
+     * @param name the user's name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * getter for the user's password
+     * @return the user's password
+     */
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    /**
+     * a getter for the user's age
+     * @return the user's age
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * A setter for the user's age
+     * @param age the user's age from 1 to 100
+     */
     public void setAge(int age) {
         this.age = age;
     }
 
+    /**
+     * A getter for the user's gender
+     * @return the user's gender
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     * A setter for the user's gender
+     * @param gender the user's gender, either male, female, or other
+     */
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    /**
+     * A getter for the user's orientation
+     * @return the user's orientation, either straight or gay
+     */
     public String getOrientation() {
         return orientation;
     }
 
+    /**
+     * A setter for the user's sexual orientation
+     * @param orientation the user's sexual orientation
+     */
     public void setOrientation(String orientation) {
         this.orientation = orientation;
     }
 
+    /**
+     * A getter for the user's location
+     * @return the user's location coordinates
+     */
     public double[] getLocation() {
         return location;
     }
 
+    /**
+     * A setter for the user's location coordinates
+     * @param location the user's location coordinates
+     */
     public void setLocation(double[] location) {
         this.location = location;
     }
 
+    /**
+     * A getter for the user's BufferedImage
+     * @return The user's image
+     */
     public BufferedImage getImage() {
         return image;
     }
 
+    /**
+     * A setter for the user's BufferedImage
+     * @param image the user's BufferedImage
+     */
     public void setImage(BufferedImage image) {
         this.image = image;
     }
 
+    /**
+     * A getter for the user's bio
+     * @return The user's bio
+     */
     public String getBio() {
         return bio;
     }
 
+    /**
+     * A setter for the user's bio
+     * @param bio the user's bio
+     */
     public void setBio(String bio) {
         this.bio = bio;
     }
 
+    /**
+     * A getter for the user's hobby list
+     * @return A list of the user's hobbies
+     */
     public List<String> getHobbies() {
         return hobbies;
     }
 
+    /**
+     * A setter for the user's hobbies
+     * @param hobbies the user's hobby list
+     */
     public void setHobbies(List<String> hobbies) {
         this.hobbies = hobbies;
     }
 
+    /**
+     * A getter for the user's social media handle
+     * @return the user's social media handle
+     */
     public String getSocialMedia() {
         return socialMedia;
     }
 
+    /**
+     * A setter for the user's social media
+     * @param socialMedia the user's social media handle
+     */
     public void setSocialMedia(String socialMedia) {
         this.socialMedia = socialMedia;
     }
 
+    /**
+     * A getter for the user's list of likes
+     * @return A list of users that the user likes
+     */
     public List<String> getLikes() {
         return likes;
     }
 
+    /**
+     * A setter for the user's list of likes
+     * @param likes the list of users that the user likes
+     */
     public void setLikes(List<String> likes) {
         this.likes = likes;
     }
