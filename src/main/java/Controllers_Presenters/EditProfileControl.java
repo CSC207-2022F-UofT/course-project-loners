02f6f4b8e73d1f970b3e8f986302a45014dc69df
Rick0317 <rick.hey.huang@gmail.com>
2022-11-28 12:03:52 -0500

    Solved some warnings

--- src/main/java/Controllers_Presenters/EditProfileControl.java
+++ src/main/java/Controllers_Presenters/EditProfileControl.java
@@ -4,10 +4,6 @@ package Controllers_Presenters;
 import Entities.Preferences;
 import Entities.Profile;
 import Use_Cases.Authenticator;
-
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class EditProfileControl{
@@ -16,7 +12,6 @@ public class EditProfileControl{
     * This class manages editing the profile.
      */
     public String name;
-    public Set<Object> objects = new HashSet<>();
     public Authenticator authenticator = new Authenticator();
 
 
@@ -24,17 +19,15 @@ public class EditProfileControl{
             "straight", null, null, "This is Rick", null, null);
     static Preferences preferences = new Preferences(20, "male", 5, 2);
     public DataSendControl dataSend = new DataSendControl();
-    public DataFetchControl dataFetchControl = new DataFetchControl();
     public  EditProfileControl(){
     }
 
+    /**
+     * This method edits changes the data of the profile based on the data; info
+     @param info represent the updated data of the user.
+     @return boolean value representing whether the user has successfully edited the profile or not.
+     */
     public boolean edit(HashMap<String, Object> info){
-        /**
-         * This method edits changes the data of the profile based on the data; info
-         @param info represent the updated data of the user.
-         @return boolean value representing whether the user has successfully edited the profile or not.
-         @throws nothing
-         */
         Object[] keys = info.keySet().toArray();
         Object[] values = info.values().toArray();
         // If there is no data in info, we will do nothing and return false
@@ -52,14 +45,14 @@ public class EditProfileControl{
                     return false;
                 }
             } else if (keys[k] == "email") {
-                if(authenticator.is_valid_email((String) values[k])){
+                if(Authenticator.is_valid_email((String) values[k])){
                     profile.setEmail( (String) values[k]);
                 } else {
                     System.out.println("Invalid Email");
                     return false;
                 }
             } else if (keys[k] == "age") {
-                if(authenticator.is_valid_age((int) values[k])){
+                if(Authenticator.is_valid_age((int) values[k])){
                     profile.setAge((int) values[k]);
                 } else {
                     return false;
@@ -89,35 +82,29 @@ public class EditProfileControl{
         return true;
     }
 
-    public boolean send(HashMap<String, Object> info) {
-        /**
-         * This method sends the given updated profile data to the database
-         @param info representing the updated data.
-         @return boolean value representing whether it has successfully sent the data to the database
-         @throws nothing
-         */
+    /**
+     * This method sends the given updated profile data to the database
+     @param info representing the updated data.
+     */
+    public void send(HashMap<String, Object> info) {
 
-        if(this.edit(info)){
+        if(this.edit(info)) {
 
             String likes;
-            if (profile.getLikes() == null){
+            if (profile.getLikes() == null) {
                 likes = null;
-            } else{
+            } else {
                 likes = String.join(": ", profile.getLikes());
             }
 
-            String str_data = profile.getName()+", "+profile.getEmail()+", "+ profile.getPassword()+", "+profile.getAge()+", "+
-                    profile.getBio()+ ", "+profile.getGender()+", "+profile.getOrientation()+", "+
-                    profile.getLocation()+", "+String.join(": ",profile.getHobbies())+", "+
-                    profile.getSocialMedia()+", "+likes+", "+preferences.getPreferredAge()+", "+
-                    preferences.getPreferredGender()+", "+preferences.getPreferredLocationRange();
+            String str_data = profile.getName() + ", " + profile.getEmail() + ", " + profile.getPassword() + ", " + profile.getAge() + ", " +
+                    profile.getBio() + ", " + profile.getGender() + ", " + profile.getOrientation() + ", " +
+                    profile.getLocation() + ", " + String.join(": ", profile.getHobbies()) + ", " +
+                    profile.getSocialMedia() + ", " + likes + ", " + preferences.getPreferredAge() + ", " +
+                    preferences.getPreferredGender() + ", " + preferences.getPreferredLocationRange();
             dataSend.send_toid(2, str_data.split(", "));
-            return true;
-        }else{
-            // If the there was a problem regarding editing the profile, the data will not be sent to database.txt
-            return false;
-        }
 
+        }
     }
 
 }
