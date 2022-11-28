f9d90a30536ad8899e3fb6088dd77e6d3270e9dd
kellyfan <kani.fan@mail.utoronto.ca>
2022-11-28 13:41:27 -0500

    moved test files

---
+++ src/test/java/Use_Cases/ConnectProfilesTest.java
@@ -0,0 +1,12 @@
+package Use_Cases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectProfilesTest {
+
+    @Test
+    void compareTraits() {
+    }
+}
