4abc2c70683b1573bf7cbb9147bc2e149ae46d5c
Amelia Riddell <amelia.riddell@mail.utoronto.ca>
2022-12-02 15:59:59 -0500

    Some minor changes to ObjectListToProfile, made it neat and added comments.

--- src/main/java/UIs/ProfileFinderUI.java
+++ src/main/java/UIs/ProfileFinderUI.java
@@ -5,7 +5,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
 import java.util.List;
 
 import Controllers_Presenters.*;
