be760ba928da99711be2e30322002eb7e9b6fd15
kellyfan <kani.fan@mail.utoronto.ca>
2022-12-02 00:12:37 -0500

    testfiles

---
+++ src/main/java/tests/Controllers_Presenters/DataSendControlTest.java
@@ -0,0 +1,16 @@
+package Controllers_Presenters;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class DataSendControlTest extends TestCase {
+    @Test
+    public void testSend_toid() {
+    }
+    @Test
+    public void testProfileConvertStr() {
+    }
+    @Test
+    public void testPreferencesConvertStr() {
+    }
+}
