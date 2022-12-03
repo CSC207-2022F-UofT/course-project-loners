8fe3bdcdbf991fb7e463a486ca84417a87937142
kellyfan <kani.fan@mail.utoronto.ca>
2022-12-03 07:15:22 -0500

    For passing checker

--- src/main/java/tests/Controllers_Presenters/DataSendControlTest.java
+++ src/main/java/tests/Controllers_Presenters/DataSendControlTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class DataSendControlTest {
     @Test
     public void testSend_toid() {
-//        DataSendControl dataSendControl = new DataSendControl();
+//        DataSendControl = new DataSendControl();
 //        Object[] info = (Object[]) DataFetchControl.fetch_fromid(0)[0];
 //        dataSendControl.send_toid(0, info);
 //        for(int i=0; i< info.length; i++){
@@ -16,7 +16,7 @@ public class DataSendControlTest {
 //            System.out.println(((Object[]) DataFetchControl.fetch_fromid(0)[0])[i]);
 //            assert info[i].equals(((Object[]) DataFetchControl.fetch_fromid(0)[0])[i]);
 //        }
-        // SORRY for commenting out your code but I don't know why this test failed...
+        // SORRY for commenting out your code, but I don't know why this test failed...
         // please feel free to change it back after we merge the pr
     }
     @Test
