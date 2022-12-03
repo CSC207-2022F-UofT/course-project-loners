619ca9a17b8f1d44944dcc61a7e8e53ffd6dd111
kellyfan <kani.fan@mail.utoronto.ca>
2022-12-03 01:47:20 -0500

    Fix warnings in RegDataStore.java

--- src/main/java/Use_Cases/RegDataStore.java
+++ src/main/java/Use_Cases/RegDataStore.java
@@ -1,14 +1,12 @@
 package Use_Cases;
 
 import Controllers_Presenters.DataSendControl;
-import Entities.Preferences;
 import Entities.Profile;
 
 public class RegDataStore {
     public boolean success;
     public RegDataStore(String social, String email, String password, String name, int age, String gender, String postcode) {
-        LocationConverter converter =  new LocationConverter();
-        double[] location = converter.codeToCoords(postcode);
+        double[] location = LocationConverter.codeToCoords(postcode);
         Profile profile = new Profile(social, email, password, name, age, gender, location);
         new DataSendControl(profile);
         this.success = true;
