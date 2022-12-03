32e97816631952913f5ffa1728d93cb05a0ff6cc
kellyfan <kani.fan@mail.utoronto.ca>
2022-12-03 15:22:16 -0500

    Edited start.java

--- src/main/java/start.java
+++ src/main/java/start.java
@@ -2,7 +2,6 @@ import Controllers_Presenters.UIController;
 
 public class start {
     public static void main(String[] args) {
-        new UIController();
         UIController.launchWelcomeUI();
     }
 }
