11af6a3ad86e99c23c1d5cb15adc3c01b957a98d
kellyfan <kani.fan@mail.utoronto.ca>
2022-12-08 08:19:15 -0500

    Changed name of ProfileGenerator.java;

--- src/main/java/controllers/RegControl.java
+++ src/main/java/controllers/RegControl.java
@@ -2,7 +2,7 @@ package controllers;
 
 import dataaccess.FetchData; // implements a Use Case interface
 import dataaccess.SendData; // implements a Use Case interface
-import usecases.ProfileFactory;
+import usecases.ProfileGenerator;
 import usecases.RegChecker;
 
 import javax.imageio.ImageIO;
@@ -84,7 +84,7 @@ public class RegControl {
      */
     private boolean regDataStore(String platform, String pfInfo,String email, String pw, String name, String age, String gender, String postcode) {
         try{ // DataSendControl did not have exception i.e. data can be stored to the database
-            new SendData(ProfileFactory.generateProfile(platform, pfInfo, email, pw, name, age, gender, postcode));
+            new SendData(ProfileGenerator.generateProfile(platform, pfInfo, email, pw, name, age, gender, postcode));
         } catch (Exception e) { // If DataSendControl have exception i.e. data not did store to the database
             return false;
         }
